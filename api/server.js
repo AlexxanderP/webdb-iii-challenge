a2b0aa1ef72bb63d102906f9a2d9129d812dc2bc
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-03-27 14:39:56 -0700

    fleshed out cohorts-router.js file with first GET handler, and imported cohortsRouter and studentsRouter into server.js and told serers to use specific endpoints

--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const helmet = require("helmet");
 
+// const cohortsRouter = require('../routers/cohorts-router.js);
+// const studentsRouter = require('../routers/students-router');
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+// server.use('/spi/cohorts', cohortsRouter);
+// server.use('/spi/students', studentsRouter);
 
 module.export = server;
